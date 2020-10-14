c8c3e5524cac1304ce09015ab0ccb5a69ca06a3e
MatthieuGenovese <matthieu.genovese@gmail.com>
2020-10-14 16:10:00 +0200

    tweak des users

--- appFormation/src/app/views/users/pages/page-list-users/page-list-users.component.ts
+++ appFormation/src/app/views/users/pages/page-list-users/page-list-users.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/core/services/user.service';
+import { Role } from 'src/app/shared/enums/role.enum';
+import { BtnI } from 'src/app/shared/interfaces/btn-i';
 import { User } from 'src/app/shared/models/user.model';
 
 @Component({
@@ -11,6 +13,8 @@ export class PageListUsersComponent implements OnInit {
 
   public collectionUser: User[];
   public collectionHeaders: string[];
+  public roles = Object.values(Role);
+  public btnAdduser: BtnI;
 
 
   constructor(
@@ -19,10 +23,15 @@ export class PageListUsersComponent implements OnInit {
 
   ngOnInit(): void {
     this.collectionHeaders = [ 'ID', 'Username', 'Role' ];
+    this.btnAdduser = { label : 'Add user', route : 'add'};
     this.us.collection.subscribe(users => {
       this.collectionUser= users;
       console.log(this.collectionUser);
     })
+  }
+
+  public updateRole(user: User, event): void{
+    
   }
 
 }
